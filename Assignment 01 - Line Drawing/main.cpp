5d08df6d0abfdb1c33e318abff5615d42935b34d
Keagon King <keagonk@gmail.com>
2025-01-09 21:40:06 -0600

    Assignment 1 - Line Drawing Completed

--- Assignment 01 - Line Drawing/main.cpp
+++ Assignment 01 - Line Drawing/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include "include/RasterSurface.h"
+#include "RasterSurface.h"
 #include <Windows.h>
+#include "Graphics_Math.h"
 #include <vector>
 #include <random>
 
@@ -9,6 +10,8 @@
 #define NUM_PIXELS	(RASTER_WIDTH * RASTER_HEIGHT)
 
 unsigned SCREEN_ARRAY[NUM_PIXELS];
+
+Math math;
 
 // Clear the color buffer to a solid color of my choice
 void ClearAndSetColorBuffer(unsigned int color) {
@@ -31,44 +34,110 @@ void DrawPixel(unsigned int xPos, unsigned int yPos, unsigned int color) {
 	// Convert pixel pos into 1d coord
 	unsigned int index = ConvertCoords(xPos, yPos, RASTER_WIDTH);
 
-	unsigned int srcA = (color >> 24) & 0xFF;
-	unsigned int srcR = (color >> 16) & 0xFF;
-	unsigned int srcG = (color >> 8) & 0xFF;
-	unsigned int srcB = color & 0xFF;
-
 	// If pixel has no alpha data, just set it to the color of the pixel without any conversion
-	if (srcA == 255) {
-		SCREEN_ARRAY[index] = color;
-		return;
+	SCREEN_ARRAY[index] = color;
+}
+
+// TODO : Function for doing the math for the Bresenham Line Algorithm (Mastery Assignment)
+void BresenhamLineAlgorithm(unsigned int _x1, unsigned int _y1, unsigned int _x2, unsigned int _y2) {
+
+}
+
+// TODO : Function for doing the math for the Midpoint Line Algorithm (Mastery Assignment)
+void MidPointLineAlgorithm(unsigned int _x1, unsigned int _y1, unsigned int _x2, unsigned int _y2) {
+
+}
+
+void ParametricLineAlgorithm(int _x1, int _y1, int _x2, int _y2, unsigned int color) {
+	int deltaX = math.delta(_x1, _x2);
+	int deltaY = math.delta(_y1, _y2);
+
+	if (deltaX > 0) {
+		for (int x = _x1; x <= _x2; x++) {
+			float ratio = static_cast<float>(x - _x1) / deltaX;
+			int y = math.lerp(_y1, _y2, ratio);
+			DrawPixel(x, y, color);
+		}
 	}
-
-
-	unsigned int destColor = SCREEN_ARRAY[index];
-	unsigned int destR = (destColor >> 16) & 0xFF;
-	unsigned int destG = (destColor >> 8) & 0xFF;
-	unsigned int destB = destColor & 0xFF;
-
-	float alpha = srcA / 255.0f;
-	unsigned int blendedR = static_cast<unsigned int>(srcR * alpha + destR * (1 - alpha));
-	unsigned int blendedG = static_cast<unsigned int>(srcG * alpha + destG * (1 - alpha));
-	unsigned int blendedB = static_cast<unsigned int>(srcB * alpha + destB * (1 - alpha));
-
-	// Fill pixel with specific color
-	SCREEN_ARRAY[index] = (0xFF << 24) | (blendedR << 16) | (blendedG << 8) | blendedB;
+	else if (deltaX < 0) {
+		for (int x = _x1; x >= _x2; x--) {
+			float ratio = static_cast<float>(x - _x1) / deltaX;
+			int y = math.lerp(_y1, _y2, ratio);
+			DrawPixel(x, y, color);
+		}
+	}
+	else {
+		if (deltaY > 0) {
+			for (int y = _y1; y <= _y2; y++) {
+				DrawPixel(_x1, y, color);
+			}
+		}
+		else {
+			for (int y = _y1; y >= _y2; y--) {
+				DrawPixel(_x1, y, color);
+			}
+		}
+	}
 }
 
-POINT GeneratePoint(int x, int y) {
-	POINT point = { x , y };
+void CreateShape(int perpendicularLength, int diagonalLength, int startingPointX, int startingPointY) {
+	unsigned int pink = 0xFFFF00FFF;
+	unsigned int yellow = 0xFFFFFF00;
+	unsigned int purple = 0xFF00FFFF;
+	unsigned int blue = 0xFF0000FF;
+	unsigned int green = 0xFF00FF00;
+	int pointZeroX = startingPointX;
+	int pointZeroY = startingPointY;
 
-	return point;
+	int pointOneX = pointZeroX + perpendicularLength;
+	int pointOneY = pointZeroY;
+
+	int pointTwoX = pointOneX + diagonalLength;
+	int pointTwoY = pointOneY + diagonalLength;
+
+	int pointThreeX = pointTwoX;
+	int pointThreeY = pointTwoY + perpendicularLength;
+
+	int pointFourX = pointThreeX - diagonalLength;
+	int pointFourY = pointThreeY + diagonalLength;
+
+	int pointFiveX = pointFourX - perpendicularLength;
+	int pointFiveY = pointFourY ;
+
+	int pointSixX = pointFiveX - diagonalLength;
+	int pointSixY = pointFiveY - diagonalLength;
+
+	int pointSevenX = pointSixX ;
+	int pointSevenY = pointSixY - perpendicularLength;
+
+	// Lines of Octagon
+	/* 01 */ ParametricLineAlgorithm(pointZeroX, pointZeroY, pointOneX, pointOneY, pink);
+	/* 02 */ ParametricLineAlgorithm(pointOneX, pointOneY, pointTwoX, pointTwoY, pink);
+	/* 03 */ ParametricLineAlgorithm(pointTwoX, pointTwoY, pointThreeX, pointThreeY, pink);
+	/* 04 */ ParametricLineAlgorithm(pointThreeX, pointThreeY, pointFourX, pointFourY, pink);
+	/* 05 */ ParametricLineAlgorithm(pointFourX, pointFourY, pointFiveX, pointFiveY, pink);
+	/* 06 */ ParametricLineAlgorithm(pointFiveX, pointFiveY, pointSixX, pointSixY, pink);
+	/* 07 */ ParametricLineAlgorithm(pointSixX, pointSixY, pointSevenX, pointSevenY, pink);
+	/* 08 */ ParametricLineAlgorithm(pointSevenX, pointSevenY, pointZeroX, pointZeroY, pink);
+
+	// Connective Lines
+	/* 01 */ ParametricLineAlgorithm(pointZeroX, pointZeroY, pointFourX, pointFourY, purple);
+	/* 01 */ ParametricLineAlgorithm(pointOneX, pointOneY, pointFiveX, pointFiveY, yellow);
+	/* 01 */ ParametricLineAlgorithm(pointTwoX, pointTwoY, pointSixX, pointSixY, blue);
+	/* 01 */ ParametricLineAlgorithm(pointThreeX, pointThreeY, pointSevenX, pointSevenY, green);
+
 }
+
+
+
 
 
 int main() {
 	RS_Initialize("Keagon King", RASTER_WIDTH, RASTER_HEIGHT);
 
 	do {
-
+		// Creates the Octagon
+		CreateShape(100, 70, 200, 125);
 
 	} while (RS_Update(SCREEN_ARRAY, NUM_PIXELS));
 
